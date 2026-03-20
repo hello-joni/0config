27be84f7089406e9da2706b7dd21980d7f5e05fb
Joni Hendrickson <contact@joni.site>
2026-03-20 13:40:07 -0700

    Add Claude

--- home.nix
+++ home.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -8,23 +13,24 @@
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+  # introduces backwards incompatible changes. You should not change this
+  # value, even if you update Home Manager. If you do want to update the value,
+  # then make sure to first check the Home Manager release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
-  #JHM TODO: Enable vulkan
+  #JHM TODO: Enable vulkan for fixing zed
   # nixGL.vulkan.enable = true;
+
+  #JHM Unfree packages
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (lib.getName pkg) [
+      "claude-code"
+    ];
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -40,12 +46,13 @@
 
     #JHM My installed packages through Home Manager itself
     micro
-    # zed-editor
-    #
 
     #JHM Language server for Nix
     nil
     nixd
+
+    #JHM unfree packages
+    claude-code
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -65,28 +72,13 @@
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jhen/etc/profile.d/hm-session-vars.sh
-  #
+  # shell provided by Home Manager.
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  #JHM Fish shell stuff
 
   #JHM Zed stuff
   # programs.zed-editor = {
