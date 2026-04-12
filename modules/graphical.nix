0dbca3071741f1b0f0bdb3f1416641bdedc5ff33
Joni Hendrickson <contact@joni.site>
2026-04-11 18:31:26 -0700

    Modularize nix config

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -1,102 +1,11 @@
 {
   config,
   pkgs,
-  lib,
   nixgl,
   ...
 }:
 
 {
-  home.username = "jhen";
-  home.homeDirectory = "/var/home/jhen";
-  home.stateVersion = "25.11"; # Don't change without reading HM release notes
-
-  programs.home-manager.enable = true;
-
-  nix = {
-    package = pkgs.nix;
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-  };
-
-  # Any unfree ;_; packages have to be specified here
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "claude-code"
-    ];
-
-  home.packages = with pkgs; [
-    micro # Lightweight text editor
-    nil # Nix language server
-    nixd # Nix language server
-    claude-code # Proprietary AI coding agent ;_;
-  ];
-
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "Joni Hendrickson";
-        email = "contact@joni.site";
-      };
-      init.defaultBranch = "main";
-    };
-  };
-
-  # Shell configuration with fish
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      fish_config theme choose base16-default
-      fish_config prompt choose default
-    '';
-  };
-
-  # Bash stays as login shell; fish is launched for interactive sessions
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
-
-  # Add new machines here when I'm done configuring them and update other configs
-  services.syncthing = {
-    enable = true;
-    settings = {
-      devices = {
-        # Phone (introducer, since its config isn't managed by Nix anyways)
-        ginger = {
-          id = "ROA5SZQ-OA33NRK-2NNBO5R-QVVW3FQ-DBFUWP6-XTQ4UKJ-M2D66T6-UAFPFAQ";
-          introducer = true;
-        };
-
-        # DigitalOcean server
-        sumac.id = "AY7LJTM-F5BRYPE-FDCXAGE-AJLP7TU-JW3PRMX-L6HX754-CK3MUGZ-KLEJWAB";
-
-        # Laptop
-        saffron.id = "T2F7ICT-EMNBQH6-TBDQ4DE-7X7J57J-QCGWIS2-VXBN4HB-LRGNZUZ-AFI5IQF";
-      };
-      folders."~/0everything" = {
-        id = "0everything";
-        devices = [
-          "ginger"
-          "sumac"
-          "saffron"
-        ];
-      };
-    };
-  };
-
-  # --- GUI ---
-
   # Needed to wrap anything GPU-accelerated, e.g. Zed
   targets.genericLinux.nixGL = {
     packages = nixgl.packages;
